cb2d72b3edf1fa3ae15ba2c751051f45f45e14e4
Werner Raath <werner123raath@gmail.com>
2017-06-02 14:39:09 +0200

    Using logger instead of print

--- flexselect/views.py
+++ flexselect/views.py
@@ -6,6 +6,7 @@ from django.contrib.auth.decorators import login_required
 
 from flexselect import (FlexSelectWidget, FlexSelectMultipleWidget, choices_from_instance, instance_from_request)
 
+import logging logger = logging.getLogger(__name__)
 
 @login_required
 def field_changed(request):
@@ -15,23 +16,23 @@ def field_changed(request):
     """
     widget = None
     hashed_name = request.POST.__getitem__('hashed_name')
-    print("Hashed name:", hashed_name)
+    logger.info("Hashed name:", hashed_name)
     if hashed_name in FlexSelectWidget.instances:
         widget = FlexSelectWidget.instances[hashed_name]
     elif hashed_name in FlexSelectMultipleWidget.instances:
         widget = FlexSelectMultipleWidget.instances[hashed_name]
     else:
-        print("No widget for hashed_name:", hashed_name)
+        logger.info("No widget for hashed_name:", hashed_name)
 
     instance = instance_from_request(request, widget)
-    print("data", request.POST.data)
+    logger.info("data", request.POST.data)
 
     if bool(int(request.POST.__getitem__('include_options'))):
-        print("include_options", request.POST.__getitem__('include_options'))
+        logger.info("include_options", request.POST.__getitem__('include_options'))
         choices = choices_from_instance(instance, widget)
         options = Select(choices=choices).render_options([])
     else:
-        print("POST does not contain attribute: 'include_options' ")
+        logger.info("POST does not contain attribute: 'include_options' ")
         options = None
 
     return HttpResponse(json.dumps({
