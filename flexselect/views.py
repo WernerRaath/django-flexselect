89bb48d076d3697f6cead7c71b0bfdbe9a26c4a1
Werner Raath <werner123raath@gmail.com>
2017-06-02 14:31:29 +0200

    Increased logging options

--- flexselect/views.py
+++ flexselect/views.py
@@ -15,17 +15,23 @@ def field_changed(request):
     """
     widget = None
     hashed_name = request.POST.__getitem__('hashed_name')
+    print("Hashed name:", hashed_name)
     if hashed_name in FlexSelectWidget.instances:
         widget = FlexSelectWidget.instances[hashed_name]
     elif hashed_name in FlexSelectMultipleWidget.instances:
         widget = FlexSelectMultipleWidget.instances[hashed_name]
+    else:
+        print("No widget for hashed_name:", hashed_name)
 
     instance = instance_from_request(request, widget)
+    print("data", request.POST.data)
 
     if bool(int(request.POST.__getitem__('include_options'))):
+        print("include_options", request.POST.__getitem__('include_options'))
         choices = choices_from_instance(instance, widget)
         options = Select(choices=choices).render_options([])
     else:
+        print("POST does not contain attribute: 'include_options' ")
         options = None
 
     return HttpResponse(json.dumps({
