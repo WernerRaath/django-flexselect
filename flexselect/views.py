967162de4b83da89a8fb565687eaf01d17a14d00
Werner Raath <werner123raath@gmail.com>
2017-06-08 14:21:48 +0200

    Logging fixes

--- flexselect/views.py
+++ flexselect/views.py
@@ -4,8 +4,10 @@ from django.http import HttpResponse
 from django.forms.widgets import Select
 from django.contrib.auth.decorators import login_required
 
-from flexselect import (FlexSelectWidget, FlexSelectMultipleWidget, choices_from_instance, instance_from_request)
+from flexselect import (FlexSelectWidget, FlexSelectMultipleWidget, choices_from_instance, instance_f
 
+import logging
+logger = logging.getLogger(__name__)
 
 @login_required
 def field_changed(request):
@@ -13,18 +15,26 @@ def field_changed(request):
     Ajax callback called when a trigger field or base field has changed. Returns
     html for new options and details for the dependent field as json.
     """
-    hashed_name = request.POST.__getitem__('hashed_name')
+    widget = None
+    logger.info(request.POST)
+    hashed_name = request.POST.get('hashed_name', "None")
+    logger.info("Hashed name:" + hashed_name)
     if hashed_name in FlexSelectWidget.instances:
         widget = FlexSelectWidget.instances[hashed_name]
     elif hashed_name in FlexSelectMultipleWidget.instances:
         widget = FlexSelectMultipleWidget.instances[hashed_name]
+    else:
+        logger.info("No widget for hashed_name:"+ hashed_name)
 
     instance = instance_from_request(request, widget)
 
-    if bool(int(request.POST.__getitem__('include_options'))):
+    include_options = request.POST.get('include_options', None)
+    if include_options:
+        logger.info("include_options", include_options)
         choices = choices_from_instance(instance, widget)
-        options = Select(choices=choices).render_options([], [])
+        options = Select(choices=choices).render_options(choices, [])
     else:
+        logger.info("POST does not contain attribute: 'include_options' ")
         options = None
 
     return HttpResponse(json.dumps({
